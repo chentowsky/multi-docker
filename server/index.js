15577fbcbbf184b1fa88ef882315ff9741a8e1b1
@chentowsky <vicente.ruiz.vasquez@gmail.com>
2020-05-10 16:33:40 +0200

    Fix error

--- server/index.js
+++ server/index.js
@@ -60,7 +60,7 @@ app.post('/values', async (req, res) => {
     return res.status(422).send('Index too high');
   }
 
-  redisClient.hset('values', indexa, 'Nothing yet!');
+  redisClient.hset('values', index, 'Nothing yet!');
   redisPublisher.publish('insert', index);
   pgClient.query('INSERT INTO values(number) VALUES($1)', [index]);
 
